78a6d90105e46635793bec42ee5aa01a82cf189d
dylanbernard <dylanbernard@hotmail.fr>
2025-08-08 11:25:03 +0900

    Recipe + ingredients data (ScriptableObject)

---
+++ Assets/Recipe/IngredientSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class IngredientSO : ScriptableObject
+{
+    public string ingredientName;
+    public Sprite icon;
+}
---
+++ Assets/Recipe/RecipeSO.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecipeSO : ScriptableObject
+{
+    public string recipeName;
+    public Sprite resultIcon;
+    public int value;
+    public List<IngredientSO> requiredIngredients;
+}
